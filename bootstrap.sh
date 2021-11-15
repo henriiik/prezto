6a97cc7116ab88e72bf52da8514fa65559906719
Henrik Sjööh <henrik.sjooh@configura.com>
2021-11-15 12:11:10 +0100

    customize

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+set -xe;
+
+git submodule update --init --recursive
+
+ln -sf $PWD "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
