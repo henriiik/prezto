b7601bb8abab8711984418f8623be2f7ae671b54
Henrik Sjööh <henriiik@gmail.com>
2022-08-16 10:43:12 +0200

    codespace customization

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,17 @@ git submodule update --init --recursive
 
 ln -sf $PWD "${ZDOTDIR:-$HOME}/.zprezto"
 
+mkdir -p ~/.config
+ln -sf ~/.zprezto/starship.toml ~/.config/starship.toml
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+touch ${ZDOTDIR:-$HOME}/.zsh_history
+ln -sf ~/.zprezto/.zsh_history ~/.zsh_history
+
 sudo chsh -s /bin/zsh vscode
+
+curl -sS https://starship.rs/install.sh | sh -s -- --force
