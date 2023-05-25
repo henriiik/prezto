a22d5e675009868ab98b38ac754eaf2dc9e7b828
Henrik Sjööh <henriiik@gmail.com>
2023-05-25 08:31:24 +0000

    disable bootstrap for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,25 @@
 #!/bin/zsh
 
-set -xe;
+# set -xe;
 
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
-ln -sf $PWD "${ZDOTDIR:-$HOME}/.zprezto"
+# ln -sf $PWD "${ZDOTDIR:-$HOME}/.zprezto"
 
-mkdir -p ~/.config
-ln -sf ~/.zprezto/starship.toml ~/.config/starship.toml
+# mkdir -p ~/.config
+# ln -sf ~/.zprezto/starship.toml ~/.config/starship.toml
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# setopt EXTENDED_GLOB
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#     ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
 
-touch ${ZDOTDIR:-$HOME}/.zsh_history
-ln -sf ~/.zprezto/.zsh_history ~/.zsh_history
+# touch ${ZDOTDIR:-$HOME}/.zsh_history
+# ln -sf ~/.zprezto/.zsh_history ~/.zsh_history
 
-sudo chsh -s /bin/zsh vscode
+# sudo chsh -s /bin/zsh vscode
 
-curl -sS https://starship.rs/install.sh | sh -s -- --force
+# curl -sS https://starship.rs/install.sh | sh -s -- --force
 
 # mkdir -p ~/.ssh
 # touch ~/.ssh/authorized_keys
